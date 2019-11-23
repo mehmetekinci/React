0dd57a0e26db721696782aa0cc8c2f01b6503c9e
mehmetekinci <mehmetekinci1989@gmail.com>
2019-11-23 22:16:20 +0100

    add build files

---
+++ week1/project/hackyourweather/build/precache-manifest.bb87a1685da89069bb2904982dd8cb41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575e9821d04081eda482bf703e540933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c26a28104766c5c26ae",
+    "url": "/static/css/main.6c995dad.chunk.css"
+  },
+  {
+    "revision": "84b8ba3306bbe46eceab",
+    "url": "/static/js/2.d8e50262.chunk.js"
+  },
+  {
+    "revision": "3c26a28104766c5c26ae",
+    "url": "/static/js/main.f7d70b6b.chunk.js"
+  },
+  {
+    "revision": "bc0f8b6bb302380371d9",
+    "url": "/static/js/runtime-main.1cb60b05.js"
+  }
+]);
---
+++ week1/react-exercises/build/precache-manifest.cb400adbc9c50444ced40f42694e36d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'ab8b2ee6fe0c877453f5f251b1c3af82',
+    url: '/index.html',
+  },
+  {
+    revision: '3529f9b796ddd1517f59',
+    url: '/static/css/main.cf313611.chunk.css',
+  },
+  {
+    revision: '9fb5d26e2cd204a8d6c0',
+    url: '/static/js/2.627daa98.chunk.js',
+  },
+  {
+    revision: '3529f9b796ddd1517f59',
+    url: '/static/js/main.69ffcaca.chunk.js',
+  },
+  {
+    revision: '9037a25d0eea88bf3304',
+    url: '/static/js/runtime-main.5ddfa969.js',
+  },
+]);
